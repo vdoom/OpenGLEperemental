5a0183fe835d9d81203751ef8e4c3aafcbaac4a7
Volodymyr KUKSYNOK <vdoom.heretic@gmail.com>
2014-04-16 23:04:47 +0300

    qq

--- OpenGLTest/ctButton.cpp
+++ OpenGLTest/ctButton.cpp
@@ -1,4 +1,7 @@
 #include "ctButton.h"
+#include "ctInput.h"
+
+#define Input m_input->GetInputHelper()
 
 ctButton::ctButton(ctShaderManager *t_shaders) : ctClickablePlane(t_shaders)
 {}
@@ -6,5 +9,19 @@ ctButton::ctButton(ctShaderManager *t_shaders, ctScene *t_scene) : ctClickablePl
 {}
 ctButton::ctButton(ctShaderManager *t_shaders, ctScene *t_scene, QOpenGLContext *t_OGLcontext) : ctClickablePlane(t_shaders, t_scene, t_OGLcontext)
 {}
-ctButton::ctButton(ctShaderManager *t_shaders, ctScene *t_scene, QOpenGLContext *t_OGLcontext, QVector3D t_AA, QVector3D t_BB, PlaneType t_type) : ctClickablePlane(t_shaders, t_scene, t_OGLcontext, t_AA, t_BB, t_type)
-{}
+ctButton::ctButton(ctShaderManager *t_shaders, ctScene *t_scene, QOpenGLContext *t_OGLcontext, QVector3D t_AA, QVector3D t_BB, PlaneType t_type, ctInput* t_input) : ctClickablePlane(t_shaders, t_scene, t_OGLcontext, t_AA, t_BB, t_type)
+{
+    m_input = t_input;
+}
+
+ctFastDelegat* ctButton::GetOnPush()
+{
+    return m_OnPush;
+}
+
+void ctButton::Update()
+{
+    ctClickablePlane::Update();
+
+    //TODO: Check click & call OnPush || OnClick
+}
--- OpenGLTest/ctButton.h
+++ OpenGLTest/ctButton.h
@@ -1,14 +1,25 @@
 #ifndef CTBUTTON_H
 #define CTBUTTON_H
 #include "ctClickablePlane.h"
+#include "ctFastDelegat.h"
+
+class ctInput;
 
 class ctButton : public ctClickablePlane
 {
+private:
+    ctFastDelegat m_OnClick;
+    ctFastDelegat m_OnPush;
+    ctInput* m_input;
 public:
     explicit ctButton(ctShaderManager *);
     ctButton(ctShaderManager *, ctScene *);
     ctButton(ctShaderManager *, ctScene *, QOpenGLContext *);
-    ctButton(ctShaderManager *, ctScene *, QOpenGLContext *, QVector3D t_AA, QVector3D t_BB, PlaneType t_type);
+    ctButton(ctShaderManager *, ctScene *, QOpenGLContext *, QVector3D t_AA, QVector3D t_BB, PlaneType t_type, ctInput*);
+
+    ctFastDelegat* GetOnPush();
+
+    virtual void Update();
 };
 
 #endif // CTBUTTON_H
